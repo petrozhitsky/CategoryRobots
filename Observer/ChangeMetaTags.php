36ec86d319ef903f36cc5092d8be3ed001145631
roman.petrozhitsky <roman.petrozhitsky@seos-club.de>
2019-11-22 15:10:04 +0200

    added several functions to Block/Otherproducts

--- Observer/ChangeMetaTags.php
+++ Observer/ChangeMetaTags.php
@@ -4,7 +4,56 @@
 namespace Netzexpert\CategoryRobots\Observer;
 
 
-class ChangeMetaTags
-{
+use Magento\Catalog\Model\Layer\Resolver;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Page\Config;
 
+class ChangeMetaTags implements ObserverInterface
+{
+    /**
+     * @var Resolver
+     */
+    private $layerResolver;
+
+    /**
+     * @var Config
+     */
+    private $pageConfig;
+
+    /**
+     * @var UrlInterface
+     */
+    private $urlInterface;
+
+    /**
+     * ChangeMetaTags constructor.
+     * @param UrlInterface $url
+     * @param Config $config
+     * @param Resolver $resolver
+     */
+    public function __construct(
+        UrlInterface $url,
+        Config $config,
+        Resolver $resolver
+    )
+    {
+        $this->layerResolver = $resolver;
+        $this->pageConfig = $config;
+        $this->urlInterface = $url;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        /*$layer = $this->layerResolver->get();
+        $activeFilters = $layer->getState()->getFilters();*/
+
+        $layoutarray = $observer->getData('layout')->getUpdate()->getHandles();
+        if ($layoutarray[1] == 'catalog_category_view') {
+            $uri = $this->urlInterface->getCurrentUrl();
+            if (/*$activeFilters or*/ stristr($uri, '?')) {
+                $this->pageConfig->setRobots('NOINDEX, FOLLOW');
+            }
+        }
+    }
 }
